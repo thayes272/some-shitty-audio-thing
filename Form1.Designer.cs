d70549adc9670513a363fa4e2417339dbbeb7210
Dax272 <49327226+Dax272@users.noreply.github.com>
2021-07-15 22:51:05 -0700

    posting project
    simple two buttons
    -play
    -browsee
    
    Its just a simple audio player using some window library

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Mp3Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(107, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WAV Player Simple\r\nBy Dax\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(107, 299);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(147, 100);
+            this.playBtn.TabIndex = 1;
+            this.playBtn.Text = "Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(302, 299);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(147, 100);
+            this.browseBtn.TabIndex = 2;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(107, 405);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(342, 100);
+            this.stopBtn.TabIndex = 3;
+            this.stopBtn.Text = "Stop\r\n(Possible restart on song)\r\n";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 586);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Audio Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+
+namespace Mp3Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SoundPlayer sound = new SoundPlayer();
+        OpenFileDialog openfile = new OpenFileDialog();
+        //Mp3FileReader sound;
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(sound.SoundLocation);
+            sound.Play();
+            if(sound == null)
+            {
+                MessageBox.Show("Please choose your song before playing!");
+            }
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            var path = string.Empty;
+
+            using (openfile)
+            {
+                openfile.Filter = "wav (*.wav)| *.wav";
+
+                if(openfile.ShowDialog() == DialogResult.OK)
+                {
+                    //get path
+                    path = openfile.FileName;
+                }
+            }
+            sound.SoundLocation = path;
+            
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            sound.Stop();
+        }
+    }
+}
